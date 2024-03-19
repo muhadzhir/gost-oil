b3829d83b3c47a036dd4a863d26edad960a57145
kasty <kastiel_0991@mail.ru>
2024-03-19 12:14:10 +0300

    revision

--- src/components/form/Form.vue
+++ src/components/form/Form.vue
@@ -1,8 +1,54 @@
 <template>
   <div>
-    <input type="text" placeholder="Имя">
-    <input type="text" placeholder="Телефон">
-    <input type="text" placeholder="Сумма">
-    <button>Ввод</button>
+    <MazInput
+      v-model="nameInput"
+      class="form-input"
+      label="Имя"
+
+
+    />
+    <MazPhoneNumberInput
+      v-model="phoneInput"
+      :noExample="true"
+      :noSearch="true"
+      :noCountrySelector="true"
+      placeholder="Телефон"
+      class="form-input"
+      country-locale="ru-RU"
+
+    />
+    <MazInputNumber
+      v-model="priceInput"
+      class="form-input"
+      placeholder="Сумма"
+      :min="5"
+      :max="10000"
+      size="md"
+      :no-buttons="true"
+      color="secondary"
+      style="width: 200px;"
+  />
+
+  <MazBtn color="info" @click="handlerSubmit">Ввод</MazBtn>
   </div>
 </template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const nameInput = ref()
+const phoneInput = ref()
+const priceInput = ref()
+
+const handlerSubmit = () => {
+  console.log(nameInput.value)
+  console.log(phoneInput.value)
+  console.log(priceInput.value)
+}
+</script>
+<style lang="scss">
+  .form{
+    &-input {
+      margin: 20px;
+    }
+  }
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
+import 'maz-ui/styles'
 
-createApp(App)
-  .use(router)
-  .mount('#app')
+import * as components from 'maz-ui/components'
+const app = createApp(App)
+Object.entries(components).forEach(([componentName, component]) => {
+  app.component(componentName, component)
+})
+app.use(router)
+app.mount('#app')
