26a58e1d080717cb42f8f28946a84df19922841a
kasty <kastiel_0991@mail.ru>
2024-07-31 15:59:45 +0300

    fix-main

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="app" >
+  <div class="app">
     <Header />
     <router-view />
     <Footer />
@@ -9,21 +9,23 @@
 
 
 <script lang="ts" setup>
-  import Header from '@/components/Header.vue'
-  import Footer from "@/components/Footer.vue";
+import Header from '@/components/Header.vue'
+import Footer from "@/components/Footer.vue";
 </script>
 <style lang="scss">
-  @import "css/index.css";
-  body {
-    //background-color: #007Ac5;
-    margin: 0;
-    padding: 0;
-    font-family: "Arial";
-    color: white;
-  }
-  .app {
-    height: 100vh;
-    display: grid;
-    grid-template-rows: 0.1fr 10fr 1fr;
-  }
+@import "css/index.css";
+
+body {
+  background-color: #007Ac5;
+  margin: 0;
+  padding: 0;
+  font-family: "Arial";
+  color: white;
+}
+
+.app {
+  height: 100vh;
+  display: grid;
+  grid-template-rows: 0.1fr 10fr 1fr;
+}
 </style>
